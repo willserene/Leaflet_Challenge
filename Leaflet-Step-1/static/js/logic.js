a6770ade1e9169de089a329335dde09f582e1d93
William Serene <willserene@gmail.com>
2021-07-13 13:25:29 -0600

    map working - edit markers and popups, add legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,9 +1,8 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
-// d3.json(queryUrl).then(function(data) {
+
 d3.json(queryUrl, function(data) {  
   console.log(data.features);
   // Once we get a response, send the data.features object to the createFeatures function
